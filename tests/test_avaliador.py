8e2db6fa65f48e59e182c10e9c920039441f51ae
Yuri Matheus <yurimatheusb@hotmail.com>
2018-10-02 16:55:20 -0300

    adiciona nomes mais descritivos aos testes

--- tests/test_avaliador.py
+++ tests/test_avaliador.py
@@ -4,7 +4,7 @@ from src.leilao.dominio import Leilao, Usuario, Lance, Avaliador
 
 class TestAvaliador(TestCase):
 
-    def test_deve_retornar_o_maior_lance_proposto_em_ordem_crescente(self):
+    def test_deve_retornar_o_maior_e_o_menor_lance_proposto_em_ordem_crescente(self):
         # cria o cenario
         gui = Usuario('Gui')
         yuri = Usuario('Yuri')
@@ -24,7 +24,7 @@ class TestAvaliador(TestCase):
         self.assertEqual(avaliador.menor_lance, 100.0)
         self.assertEqual(avaliador.maior_lance, 200.0)
 
-    def test_deve_retornar_o_maior_lance_proposto_em_ordem_decrescente(self):
+    def test_deve_retornar_o_maior_e_o_menor_lance_proposto_em_ordem_decrescente(self):
         # cria o cenario
         gui = Usuario('Gui')
         yuri = Usuario('Yuri')
