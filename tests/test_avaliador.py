61e1807fc3b73fbf95a6486e56e77f67dd736089
Yuri Matheus <yurimatheusb@hotmail.com>
2018-10-02 16:53:00 -0300

    quebra metodo de teste

--- tests/test_avaliador.py
+++ tests/test_avaliador.py
@@ -23,3 +23,23 @@ class TestAvaliador(TestCase):
 
         self.assertEqual(avaliador.menor_lance, 100.0)
         self.assertEqual(avaliador.maior_lance, 200.0)
+
+    def test_avalia1(self):
+        # cria o cenario
+        gui = Usuario('Gui')
+        yuri = Usuario('Yuri')
+
+        leilao_celular = Leilao('Celular')
+
+        lance_do_gui = Lance(gui, 200.0)
+        lance_do_yuri = Lance(yuri, 100.0)
+
+        leilao_celular.lances.append(lance_do_gui)
+        leilao_celular.lances.append(lance_do_yuri)
+
+        # executa
+        avaliador = Avaliador()
+        avaliador.avalia(leilao_celular)
+
+        self.assertEqual(avaliador.menor_lance, 100.0)
+        self.assertEqual(avaliador.maior_lance, 200.0)
