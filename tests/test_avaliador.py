f5148d87cfa66f82bc17c15d59a8ee7f1598811c
Yuri Matheus <yurimatheusb@hotmail.com>
2018-10-02 16:52:06 -0300

    cria teste para a classe avaliador

---
+++ tests/test_avaliador.py
@@ -0,0 +1,25 @@
+from unittest import TestCase
+from src.leilao.dominio import Leilao, Usuario, Lance, Avaliador
+
+
+class TestAvaliador(TestCase):
+
+    def test_avalia(self):
+        # cria o cenario
+        gui = Usuario('Gui')
+        yuri = Usuario('Yuri')
+
+        leilao_celular = Leilao('Celular')
+
+        lance_do_gui = Lance(gui, 200.0)
+        lance_do_yuri = Lance(yuri, 100.0)
+
+        leilao_celular.lances.append(lance_do_yuri)
+        leilao_celular.lances.append(lance_do_gui)
+
+        # executa
+        avaliador = Avaliador()
+        avaliador.avalia(leilao_celular)
+
+        self.assertEqual(avaliador.menor_lance, 100.0)
+        self.assertEqual(avaliador.maior_lance, 200.0)
