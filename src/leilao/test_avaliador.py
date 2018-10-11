00885eff10da8c990c845add18c51f35917e6f4d
Yuri Matheus <yurimatheusb@hotmail.com>
2018-10-11 18:04:59 -0300

    renomeia os metodos de teste

--- src/leilao/test_avaliador.py
+++ src/leilao/test_avaliador.py
@@ -4,7 +4,7 @@ from src.leilao.dominio import Usuario, Leilao, Lance, Avaliador
 
 
 class TestAvaliador(TestCase):
-    def test_avalia(self):
+    def test_deve_retornar_o_maior_e_o_menor_valor_de_um_lance_quando_adicionados_em_ordem_crescente(self):
         gui = Usuario('Gui')
         yuri = Usuario('Yuri')
 
@@ -15,6 +15,30 @@ class TestAvaliador(TestCase):
 
         leilao_celular.lances.append(lance_do_yuri)
         leilao_celular.lances.append(lance_do_gui)
+
+        menor_lance_esperado = 100.0
+        maior_lance_esperado = 200.0
+
+        avaliador = Avaliador()
+        avaliador.avalia(leilao_celular)
+
+        menor_lance_recebido = avaliador.menor_lance
+        maior_lance_recebido = avaliador.maior_lance
+
+        self.assertEqual(menor_lance_recebido, menor_lance_esperado)
+        self.assertEqual(maior_lance_recebido, maior_lance_esperado)
+
+    def test_deve_retornar_o_maior_e_o_menor_valor_de_um_lance_quando_adicionados_em_ordem_decrescente(self):
+        gui = Usuario('Gui')
+        yuri = Usuario('Yuri')
+
+        leilao_celular = Leilao('Celular')
+
+        lance_do_gui = Lance(gui, 200.0)
+        lance_do_yuri = Lance(yuri, 100.0)
+
+        leilao_celular.lances.append(lance_do_gui)
+        leilao_celular.lances.append(lance_do_yuri)
 
         menor_lance_esperado = 100.0
         maior_lance_esperado = 200.0
