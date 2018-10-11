a5f5b7e56c6583686bb1d22431bd6497ec44558d
Yuri Matheus <yurimatheusb@hotmail.com>
2018-10-11 20:03:21 -0300

    cria mais metodos de teste

--- src/leilao/test_avaliador.py
+++ src/leilao/test_avaliador.py
@@ -51,3 +51,50 @@ class TestAvaliador(TestCase):
 
         self.assertEqual(menor_lance_recebido, menor_lance_esperado)
         self.assertEqual(maior_lance_recebido, maior_lance_esperado)
+
+    def test_deve_retornar_o_mesmo_valor_para_maior_e_menor_lance_quando_leilao_tiver_apenas_um_lance(self):
+        yuri = Usuario('Yuri')
+
+        leilao_celular = Leilao('Celular')
+
+        lance_do_yuri = Lance(yuri, 100.0)
+
+        leilao_celular.lances.append(lance_do_yuri)
+
+        menor_lance_esperado = 100.0
+        maior_lance_esperado = 100.0
+
+        avaliador = Avaliador()
+        avaliador.avalia(leilao_celular)
+
+        menor_lance_recebido = avaliador.menor_lance
+        maior_lance_recebido = avaliador.maior_lance
+
+        self.assertEqual(menor_lance_recebido, menor_lance_esperado)
+        self.assertEqual(maior_lance_recebido, maior_lance_esperado)
+
+    def test_deve_retornar_o_maior_e_o_menor_valor_de_um_lance_quando_o_leilao_tiver_tres_lances(self):
+        gui = Usuario('Gui')
+        yuri = Usuario('Yuri')
+
+        leilao_celular = Leilao('Celular')
+
+        lance_do_gui = Lance(gui, 200.0)
+        lance_do_yuri = Lance(yuri, 100.0)
+        lance_do_yuri2 = Lance(yuri, 300.0)
+
+        leilao_celular.lances.append(lance_do_yuri)
+        leilao_celular.lances.append(lance_do_gui)
+        leilao_celular.lances.append(lance_do_yuri2)
+
+        menor_lance_esperado = 100.0
+        maior_lance_esperado = 300.0
+
+        avaliador = Avaliador()
+        avaliador.avalia(leilao_celular)
+
+        menor_lance_recebido = avaliador.menor_lance
+        maior_lance_recebido = avaliador.maior_lance
+
+        self.assertEqual(maior_lance_recebido, maior_lance_esperado)
+        self.assertEqual(menor_lance_recebido, menor_lance_esperado)
