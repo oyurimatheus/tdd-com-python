11035de78beb7e3b5f1df7c304adfca3e29c2ccb
Yuri Matheus <yurimatheusb@hotmail.com>
2018-10-15 21:10:22 -0300

    altera testes para utilizar o metodo propoe

--- src/leilao/test_avaliador.py
+++ src/leilao/test_avaliador.py
@@ -16,8 +16,8 @@ class TestAvaliador(TestCase):
         lance_do_gui = Lance(gui, 200.0)
         lance_do_yuri = Lance(self.yuri, 100.0)
 
-        self.leilao.lances.append(lance_do_yuri)
-        self.leilao.lances.append(lance_do_gui)
+        self.leilao.propoe(lance_do_yuri)
+        self.leilao.propoe(lance_do_gui)
 
         menor_lance_esperado = 100.0
         maior_lance_esperado = 200.0
@@ -28,8 +28,8 @@ class TestAvaliador(TestCase):
         menor_lance_recebido = avaliador.menor_lance
         maior_lance_recebido = avaliador.maior_lance
 
-        self.assertEqual(menor_lance_recebido, menor_lance_esperado)
-        self.assertEqual(maior_lance_recebido, maior_lance_esperado)
+        self.assertEqual(menor_lance_esperado, menor_lance_recebido)
+        self.assertEqual(maior_lance_esperado, maior_lance_recebido)
 
     def test_deve_retornar_o_maior_e_o_menor_valor_de_um_lance_quando_adicionados_em_ordem_decrescente(self):
         gui = Usuario('Gui')
@@ -37,8 +37,8 @@ class TestAvaliador(TestCase):
         lance_do_gui = Lance(gui, 200.0)
         lance_do_yuri = Lance(self.yuri, 100.0)
 
-        self.leilao.lances.append(lance_do_gui)
-        self.leilao.lances.append(lance_do_yuri)
+        self.leilao.propoe(lance_do_gui)
+        self.leilao.propoe(lance_do_yuri)
 
         menor_lance_esperado = 100.0
         maior_lance_esperado = 200.0
@@ -49,13 +49,13 @@ class TestAvaliador(TestCase):
         menor_lance_recebido = avaliador.menor_lance
         maior_lance_recebido = avaliador.maior_lance
 
-        self.assertEqual(menor_lance_recebido, menor_lance_esperado)
-        self.assertEqual(maior_lance_recebido, maior_lance_esperado)
+        self.assertEqual(menor_lance_esperado, menor_lance_recebido)
+        self.assertEqual(maior_lance_esperado, maior_lance_recebido)
 
     def test_deve_retornar_o_mesmo_valor_para_maior_e_menor_lance_quando_leilao_tiver_apenas_um_lance(self):
         lance_do_yuri = Lance(self.yuri, 100.0)
 
-        self.leilao.lances.append(lance_do_yuri)
+        self.leilao.propoe(lance_do_yuri)
 
         menor_lance_esperado = 100.0
         maior_lance_esperado = 100.0
@@ -66,8 +66,8 @@ class TestAvaliador(TestCase):
         menor_lance_recebido = avaliador.menor_lance
         maior_lance_recebido = avaliador.maior_lance
 
-        self.assertEqual(menor_lance_recebido, menor_lance_esperado)
-        self.assertEqual(maior_lance_recebido, maior_lance_esperado)
+        self.assertEqual(menor_lance_esperado, menor_lance_recebido)
+        self.assertEqual(maior_lance_esperado, maior_lance_recebido)
 
     def test_deve_retornar_o_maior_e_o_menor_valor_de_um_lance_quando_o_leilao_tiver_tres_lances(self):
         gui = Usuario('Gui')
@@ -76,9 +76,9 @@ class TestAvaliador(TestCase):
         lance_do_yuri = Lance(self.yuri, 100.0)
         lance_do_yuri2 = Lance(self.yuri, 300.0)
 
-        self.leilao.lances.append(lance_do_yuri)
-        self.leilao.lances.append(lance_do_gui)
-        self.leilao.lances.append(lance_do_yuri2)
+        self.leilao.propoe(lance_do_yuri)
+        self.leilao.propoe(lance_do_gui)
+        self.leilao.propoe(lance_do_yuri2)
 
         menor_lance_esperado = 100.0
         maior_lance_esperado = 300.0
@@ -89,5 +89,5 @@ class TestAvaliador(TestCase):
         menor_lance_recebido = avaliador.menor_lance
         maior_lance_recebido = avaliador.maior_lance
 
-        self.assertEqual(maior_lance_recebido, maior_lance_esperado)
-        self.assertEqual(menor_lance_recebido, menor_lance_esperado)
+        self.assertEqual(maior_lance_esperado, maior_lance_recebido)
+        self.assertEqual(menor_lance_esperado, menor_lance_recebido)
